efadcdc0e0b4d8185837ef11766b95655c2156ab
windowsazure <windowsazure>
2016-07-23 20:45:09 +0000

    Initial Commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\gallery-hugues-extracted\DjangoFx-1.7\DjangoFX\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
